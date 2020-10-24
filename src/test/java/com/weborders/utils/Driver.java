d9c6c16b1fcd35c54740f905647ec359b1160fcf
Zarina92c <zkurtbedinova@gmail.com>
2020-10-24 16:11:01 -0400

    src

--- src/test/java/com/weborders/utils/Driver.java
+++ src/test/java/com/weborders/utils/Driver.java
@@ -5,7 +5,21 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+
 public class Driver {
+
+    //reference variable, pointer on object
+    //if reference variable doesn't point on any object it will have a null value\
+    //whenever we type new Class() - we create a new object
+    //reference type can be class, parent class, or interface implemented by class or parent class
+    //since WebDriver is an interface implemented by RemoteWebDriver class
+    //and RemoteWebDriver is a parent class of ChromeDriver, FirefoxDriver, etc.. classes
+    //we can specify reference type as a WebDriver
+    //it helps to define object type in a run time (polymorphism)
+
+
     private static WebDriver driver;
 
     private Driver() {
